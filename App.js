acdf488c3bfa2fcf95fdfe1f1e2298635a42fd75
Naoshi Hoshi <hoshinaoshi@gmail.com>
2019-10-09 22:43:04 +0900

    Touchablesコンポーネントサンプル

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View } from 'react-native';
 
 export default class App extends React.Component {
+  _onPressButton() {
+    alert("タップしました")
+  }
+
+  _onLongPressButton() {
+    alert("ロングタップしました")
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableHighlight</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableOpacity onPress={this._onPressButton}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableOpacity</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableNativeFeedback
+          onPress={this._onPressButton}
+          background={TouchableNativeFeedback.SelectableBackground()}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableNativeFeedback (Android only)</Text>
+          </View>
+        </TouchableNativeFeedback>
+        <TouchableWithoutFeedback
+          onPress={this._onPressButton}
+        >
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <TouchableHighlight onPress={this._onPressButton} onLongPress={this._onLongPressButton} underlayColor="white">
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Touchable with Long Press</Text>
+          </View>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -13,9 +49,18 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 64,
+    alignItems: "center"
   },
+  button: {
+    marginBottom: 32,
+    width: 240,
+    alignItems: "center",
+    backgroundColor: "skyblue"
+  },
+  buttonText: {
+    textAlign: "center",
+    padding: 24,
+    color: "white"
+  }
 });
