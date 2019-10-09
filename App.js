73e74573ee8a02b23341979df6f4d7a6855b3678
Naoshi Hoshi <hoshinaoshi@gmail.com>
2019-10-09 22:31:19 +0900

    Buttonコンポーネントサンプル

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Button
+          onPress={() => {alert("onPress")}}
+          title="ボタンA"
+        />
+        <Button
+          color="red"
+          onPress={() => {alert("onPress")}}
+          title="ボタンB"
+        />
+        <Button
+          disabled={true}
+          onPress={() => {alert("onPress")}}
+          title="ボタンC"
+        />
       </View>
     );
   }
