408245fd0003a2cc2f06f5e405910f8bf8e22806
Naoshi Hoshi <hoshinaoshi@gmail.com>
2019-10-09 22:08:38 +0900

    Viewコンポーネントのサンプルコード

--- App.js
+++ App.js
@@ -4,18 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+        <View>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
