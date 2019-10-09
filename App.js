c07a7ff9602cf4297be4c6be6838ce2d95d77b19
Naoshi Hoshi <hoshinaoshi@gmail.com>
2019-10-09 22:21:47 +0900

    TextInputコンポーネントサンプル

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Alert, TextInput, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: ""};
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TextInput
+          style={{ width: 200, borderColor: "black", borderWidth: 1 }}
+          value={this.state.text}
+          onChangeText={(text) => this.setState({name: text})}
+          onSubmitEditing={() => Alert.alert("onSubmitEditing")}
+        />
       </View>
     );
   }
