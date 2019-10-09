d3f4844fe5b2480116f5365f9d858ba6bafe10d2
Naoshi Hoshi <hoshinaoshi@gmail.com>
2019-10-09 23:46:40 +0900

    flexDirectionサンプル

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, flexDirection: "column"}}>
+          <View style={{width: 50, height: 50, backgroundColor: "red"}} />
+          <View style={{width: 50, height: 50, backgroundColor: "yellow"}} />
+          <View style={{width: 50, height: 50, backgroundColor: "green"}} />
+        </View>
+        <View style={{flex: 1, flexDirection: "row"}}>
+          <View style={{width: 50, height: 50, backgroundColor: "red"}} />
+          <View style={{width: 50, height: 50, backgroundColor: "yellow"}} />
+          <View style={{width: 50, height: 50, backgroundColor: "green"}} />
+        </View>
+        <View style={{flex: 1, flexDirection: "column-reverse"}}>
+          <View style={{width: 50, height: 50, backgroundColor: "red"}} />
+          <View style={{width: 50, height: 50, backgroundColor: "yellow"}} />
+          <View style={{width: 50, height: 50, backgroundColor: "green"}} />
+        </View>
+        <View style={{flex: 1, flexDirection: "row-reverse"}}>
+          <View style={{width: 50, height: 50, backgroundColor: "red"}} />
+          <View style={{width: 50, height: 50, backgroundColor: "yellow"}} />
+          <View style={{width: 50, height: 50, backgroundColor: "green"}} />
+        </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
